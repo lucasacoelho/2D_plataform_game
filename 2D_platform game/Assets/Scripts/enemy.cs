d15e17a000ded1f9727316aca22e673d40f24d19
Lucas Almeida Coelho <lucas.acoelho@hotmail.com>
2024-12-10 00:42:27 -0300

    life bar working

--- 2D_platform game/Assets/Scripts/enemy.cs
+++ 2D_platform game/Assets/Scripts/enemy.cs
@@ -8,6 +8,7 @@ public class enemy : MonoBehaviour
     public GameObject lifebar;
 
     public float lifepoints = 200f;
+    public float momentlife;
     public GameObject bullet;
     public GameObject meele;
     public float dano;
@@ -15,34 +16,57 @@ public class enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        momentlife = lifepoints;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (lifepoints <= 0f)
+        if (momentlife <= 0f)
         {
             Destroy(this.gameObject);
         }
+
+        while (momentlife < lifepoints)
+        {
+            healing();
+            scalelife();  
+        }
+        
     }
     private void OnCollisionEnter2D (Collision2D GetHit) 
     {
         if( GetHit.gameObject.CompareTag("Shot"))
         {
             dano = bullet.GetComponent<Bullet>().damage;
-            lifepoints -= dano;
+            
+            momentlife -= dano;
             clone = GameObject.Find("/Clone");
             Debug.Log(clone);
             //Destroy(clone.gameObject);
-            Debug.Log("vida " +  lifepoints);
+            Debug.Log("vida " +  momentlife);
+            scalelife();
         }
 
         if (GetHit.gameObject.CompareTag("Meele Atack"))
         {
             dano = meele.GetComponent<atack>().damage;
-            lifepoints -= dano;
+            momentlife -= dano;
             Debug.Log("vida " +  lifepoints);
+            scalelife();
         }
+    }
+
+    private void scalelife()
+    {
+        float size = (momentlife / lifepoints) * 100f;
+        lifebar.transform.localScale = new Vector3(size, 5.679343f, 1f);
+
+    }
+
+    private IEnumerator healing()
+    {
+        yield return new WaitForSeconds(1f);
+        momentlife += 1f;
     }
 }
